5e31b3d4deabede3cb8a88b2469de62246670532
galang48 <galangyudha1945@gmail.com>
2024-06-04 10:35:56 +0700

    menyelesaikan project stream reader and writer

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -34,5 +34,20 @@ int main() {
     infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file " << endl; 
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << "\n";
+        }
+        //tutup file tersebut setelah seesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
 
