ddddbdf29f9f00d41e14274dd15bb77d3edbba1d
galang48 <galangyudha1945@gmail.com>
2024-06-04 10:57:29 +0700

    menambahkan Exception Handling

--- ManualExeption/ManualExeption.cpp
+++ ManualExeption/ManualExeption.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuuah integer maka
+		cout << "Pertnyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << " Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
 }
 
 
