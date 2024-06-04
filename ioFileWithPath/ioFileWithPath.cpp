0d84e721bbd73a1113248c1d112f045ef60303c3
galang48 <galangyudha1945@gmail.com>
2024-06-04 10:43:42 +0700

    Nambahin input nama file sama penulisan baris ke file, ketik 'q' buat keluar

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -7,6 +7,23 @@ int main() {
     string baris;
     string NamaFile;
 
+    cout << "Masukan nama file : ";
+    cin >> NamaFile;
 
+    /// membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk kesebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
 
