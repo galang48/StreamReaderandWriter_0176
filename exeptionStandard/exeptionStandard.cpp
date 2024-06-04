909d201689dc5164b174ea4cfb6dff76d0573581
galang48 <galangyudha1945@gmail.com>
2024-06-04 11:15:22 +0700

    menyelesaikan project keempat

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <exception>
-//untuk obyek exception yang akan digunakan
+//untuk objek exception yang akan digunakan
 #include <array>
-//untuk obyek array yang akan kita gunakan
+//untuk objek array yang akan digunakan
 using namespace std;
 
-int main()
-{
-    
+int main() {
+	cout << "Awal Program" << endl; //penanda 1:....
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array int 3 elemen
+		//cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan objek exception
+		cout << e.what() << endl;
+		//akan dieksekusi karna array data hanya memiliki 3 elemen
+	}
+	cout << "Baris Program yang terakhir" << endl;
+	//penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	return 0;
 }
-
